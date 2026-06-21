6a773afd5b10646d6e66819f0f8be4bb2de95cc3
rainbow-dash-15010537707 <15010537707@139.com>
2026-06-21 18:48:11 +0800

    Restore packaged native files

--- build/afterPack.cjs
+++ build/afterPack.cjs
@@ -1,12 +1,3 @@
-const fs = require('node:fs');
-const path = require('node:path');
-
 module.exports = async function afterPack(context) {
-  const unpackedNodeModules = path.join(context.appOutDir, 'resources', 'app.asar.unpacked', 'node_modules');
-  removeIfExists(path.join(unpackedNodeModules, '@napi-rs'));
+  void context;
 };
-
-function removeIfExists(target) {
-  if (!fs.existsSync(target)) return;
-  fs.rmSync(target, { recursive: true, force: true });
-}
